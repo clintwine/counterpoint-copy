2c0f2b1c59cd5799954d0d43a7806f1370251bd3
Arshia K <youthinkasido@gmail.com>
2025-11-28 01:32:34 +0000

    File changes

--- src/components/counterpoint/NoteGrid.jsx
+++ src/components/counterpoint/NoteGrid.jsx
@@ -843,9 +843,13 @@ export default function NoteGrid({
           </div>
 
           <div 
-          ref={gridRef}
-          className="overflow-auto max-h-[50vh] sm:max-h-[400px] relative select-none mx-2 sm:mx-5"
-        style={{ scrollbarWidth: 'thin', scrollbarColor: '#475569 transparent' }}
+                  ref={gridRef}
+                  className="overflow-auto max-h-[50vh] sm:max-h-[400px] relative select-none mx-2 sm:mx-5"
+                style={{ 
+                  scrollbarWidth: 'thin', 
+                  scrollbarColor: '#475569 transparent',
+                  touchAction: tool === 'marquee' ? 'none' : 'auto'
+                }}
         onMouseMove={handlePointerMove}
                       onMouseUp={handlePointerUp}
                       onMouseLeave={handlePointerUp}
