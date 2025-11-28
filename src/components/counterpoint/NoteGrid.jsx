a3f3af4771931858d559747110cf95381ef4ec44
Arshia K <youthinkasido@gmail.com>
2025-11-28 01:23:21 +0000

    File changes

--- src/components/counterpoint/NoteGrid.jsx
+++ src/components/counterpoint/NoteGrid.jsx
@@ -895,8 +895,8 @@ export default function NoteGrid({
         onScroll={(e) => setViewportState({ scrollLeft: e.target.scrollLeft, scrollTop: e.target.scrollTop })}
       >
         <div className="inline-flex min-w-full" ref={containerRef}>
-          {/* Pitch labels - fixed column on left */}
-                                      <div className="sticky left-0 z-20 flex-shrink-0" style={{ backgroundColor: 'rgb(30, 41, 59)' }}>
+          {/* Pitch labels - fixed column on left, allows vertical scrolling */}
+                                      <div className="sticky left-0 z-20 flex-shrink-0" style={{ backgroundColor: 'rgb(30, 41, 59)', touchAction: 'pan-y' }}>
                                         <div className="h-7 border-b border-slate-600 bg-slate-800 sticky top-0 z-30" />
                           {pitches.map((pitch) => {
                                                                         const isSharp = pitch.includes('#');
