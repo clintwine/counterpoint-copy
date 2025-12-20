72b8072207dd964949c813f98718bf08894fa6ed
Arshia K <youthinkasido@gmail.com>
2025-12-20 21:16:19 +0000

    File changes

--- src/components/counterpoint/NoteGrid.jsx
+++ src/components/counterpoint/NoteGrid.jsx
@@ -1345,7 +1345,7 @@ export default function NoteGrid({
                   className={`overflow-auto relative select-none mx-2 sm:mx-5 ${
                     showPianoPanel 
                       ? (selectedNotes.size > 0 ? 'max-h-[48vh] sm:max-h-[440px]' : 'max-h-[56vh] sm:max-h-[520px]')
-                      : (selectedNotes.size > 0 ? 'max-h-[74vh] sm:max-h-[665px]' : 'max-h-[81vh] sm:max-h-[725px]')
+                      : (selectedNotes.size > 0 ? 'max-h-[68vh] sm:max-h-[615px]' : 'max-h-[75vh] sm:max-h-[675px]')
                   }`}
                 style={{ 
                   scrollbarWidth: 'thin', 
