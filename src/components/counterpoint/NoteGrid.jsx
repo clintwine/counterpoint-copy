2058cfdd4474630fcfeaa84f32e7932ceba57715
Arshia K <youthinkasido@gmail.com>
2025-12-20 22:01:53 +0000

    File changes

--- src/components/counterpoint/NoteGrid.jsx
+++ src/components/counterpoint/NoteGrid.jsx
@@ -1344,8 +1344,8 @@ export default function NoteGrid({
                   ref={gridRef}
                   className={`overflow-auto relative select-none mx-2 sm:mx-5 ${
                     showPianoPanel 
-                      ? (selectedNotes.size > 0 ? 'max-h-[56vh] sm:max-h-[540px]' : 'max-h-[56vh] sm:max-h-[520px]')
-                      : (selectedNotes.size > 0 ? 'max-h-[75vh] sm:max-h-[695px]' : 'max-h-[75vh] sm:max-h-[675px]')
+                      ? (selectedNotes.size > 0 ? 'max-h-[56vh] sm:max-h-[530px]' : 'max-h-[56vh] sm:max-h-[520px]')
+                      : (selectedNotes.size > 0 ? 'max-h-[75vh] sm:max-h-[685px]' : 'max-h-[75vh] sm:max-h-[675px]')
                   }`}
                 style={{ 
                   scrollbarWidth: 'thin', 
