8404c2df3d37e8475f0e6fc2073bbf7bc7a5583d
Arshia K <youthinkasido@gmail.com>
2025-12-21 16:32:39 +0000

    File changes

--- src/components/counterpoint/PianoKeyboard.jsx
+++ src/components/counterpoint/PianoKeyboard.jsx
@@ -745,10 +745,10 @@ export default function PianoKeyboard({ activeNotes = [], instrument = 'organ', 
             </Button>
           </div>
         </div>
-      </div>
-      </div>
+        </div>
+        </div>
 
-      <div className="overflow-x-auto pb-1">
+        <div className="overflow-x-auto pb-1">
         <div className="inline-flex items-center gap-3">
           <div className="relative" style={{ width: totalWidth + whiteKeyWidth * 3, height: 80 }}>
             {/* Generate all 88 keys - A0 to C8 */}
