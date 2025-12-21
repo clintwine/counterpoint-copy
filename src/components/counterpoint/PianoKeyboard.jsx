55d10f2f84921cd24c0bdf8461a9402d57f58dd7
Arshia K <youthinkasido@gmail.com>
2025-12-21 16:25:12 +0000

    File changes

--- src/components/counterpoint/PianoKeyboard.jsx
+++ src/components/counterpoint/PianoKeyboard.jsx
@@ -935,19 +935,41 @@ export default function PianoKeyboard({ activeNotes = [], instrument = 'organ', 
             );
             whiteKeyIndex++;
             
-            // Add just one extra greyed out key at the end to fill space
-            keys.push(
-              <div
-                key="extra-end"
-                className="absolute bottom-0 border border-slate-400 rounded-b opacity-30 cursor-not-allowed"
-                style={{
-                  left: whiteKeyIndex * whiteKeyWidth,
-                  width: whiteKeyWidth - 1,
-                  height: 75,
-                  backgroundColor: '#D0D0D0',
-                }}
-              />
-            );
+            // Add 5 extra greyed out keys at the end to fill space
+            const extraHighKeys = ['C#', 'D', 'D#', 'E', 'F'];
+            extraHighKeys.forEach((note, idx) => {
+              const isBlack = note.includes('#');
+              if (isBlack) {
+                // Black key
+                keys.push(
+                  <div
+                    key={`extra-high-black-${note}`}
+                    className="absolute top-0 rounded-b z-10 opacity-30 cursor-not-allowed"
+                    style={{
+                      left: (whiteKeyIndex - 1) * whiteKeyWidth + (note === 'C#' ? 14 : 38) * (whiteKeyWidth / 24),
+                      width: blackKeyWidth,
+                      height: 45,
+                      backgroundColor: '#3A3A3A',
+                    }}
+                  />
+                );
+              } else {
+                // White key
+                keys.push(
+                  <div
+                    key={`extra-high-${note}`}
+                    className="absolute bottom-0 border border-slate-400 rounded-b opacity-30 cursor-not-allowed"
+                    style={{
+                      left: whiteKeyIndex * whiteKeyWidth,
+                      width: whiteKeyWidth - 1,
+                      height: 75,
+                      backgroundColor: '#D0D0D0',
+                    }}
+                  />
+                );
+                whiteKeyIndex++;
+              }
+            });
             
             return keys;
           })()}
