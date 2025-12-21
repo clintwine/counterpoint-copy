005d9534837c9ef4536f3f635182662c5cd4862f
Arshia K <youthinkasido@gmail.com>
2025-12-21 15:47:30 +0000

    File changes

--- src/components/counterpoint/PlaybackControls.jsx
+++ src/components/counterpoint/PlaybackControls.jsx
@@ -229,20 +229,22 @@ export default function PlaybackControls({
           <div className="w-px h-6 bg-[#3A3A3A]" />
 
           {/* Time Signature */}
-          <div className="flex flex-col items-start justify-center">
-            <Select value={timeSignature} onValueChange={onTimeSignatureChange}>
-              <SelectTrigger className="h-auto w-12 bg-transparent border-none text-white text-sm font-medium px-1 hover:bg-[#2D2D2D] py-0 -mt-1">
-                <SelectValue />
-              </SelectTrigger>
-              <SelectContent className="bg-[#2D2D2D] border-[#3A3A3A]">
-                {TIME_SIGNATURES.map(ts => (
-                  <SelectItem key={ts.value} value={ts.value} className="text-white text-sm">
-                    {ts.label}
-                  </SelectItem>
-                ))}
-              </SelectContent>
-            </Select>
-            <span className="text-white/40 text-[9px] uppercase tracking-wider">SIG</span>
+          <div className="flex items-center">
+            <div className="w-12 flex flex-col items-center">
+              <Select value={timeSignature} onValueChange={onTimeSignatureChange}>
+                <SelectTrigger className="h-auto w-12 bg-transparent border-none text-white text-sm font-medium px-1 hover:bg-[#2D2D2D] py-0.5">
+                  <SelectValue />
+                </SelectTrigger>
+                <SelectContent className="bg-[#2D2D2D] border-[#3A3A3A]">
+                  {TIME_SIGNATURES.map(ts => (
+                    <SelectItem key={ts.value} value={ts.value} className="text-white text-sm">
+                      {ts.label}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+              <span className="text-white/40 text-[9px] uppercase tracking-wider">SIG</span>
+            </div>
           </div>
         </div>
 
