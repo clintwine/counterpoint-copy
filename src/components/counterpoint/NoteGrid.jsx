eaf27df6e2692770771d1204aae136ccb99ba55c
Arshia K <youthinkasido@gmail.com>
2026-03-21 23:29:16 +0000

    File changes

--- src/components/counterpoint/NoteGrid.jsx
+++ src/components/counterpoint/NoteGrid.jsx
@@ -2788,7 +2788,7 @@ export default function NoteGrid({
                                       className="absolute rounded flex items-center justify-start pl-1 shadow-lg pointer-events-none z-10"
                                       style={{
                                         left: 56 + newBeat * CELL_WIDTH + 2,
-                                        top: 28 + newPitchIdx * CELL_HEIGHT + 2,
+                                        top: 28 + newPitchIdx * CELL_HEIGHT - viewportState.scrollTop + 2,
                                         width: noteWidth,
                                         height: CELL_HEIGHT - 4,
                                         backgroundColor: velocityColor,
