cd41a5ae644350c0d5c887e65f42ca9d44778610
Arshia K <youthinkasido@gmail.com>
2026-03-25 21:37:03 +0000

    File changes

--- src/components/counterpoint/GridOverlays.jsx
+++ src/components/counterpoint/GridOverlays.jsx
@@ -33,7 +33,7 @@ export default function GridOverlays({
       <div
         className="absolute z-30 pointer-events-none"
         style={{
-          left: `${gridRect.left + 56 + smoothPlayhead * CELL_WIDTH - viewportState.scrollLeft}px`,
+          left: `${gridRect.left + smoothPlayhead * CELL_WIDTH - viewportState.scrollLeft}px`,
           top: 28,
           transform: 'translateX(-50%)',
           width: Math.max(2, 3 * zoom),
