2163598483be2070333cb594d7e3f5c26e135a0f
Arshia K <youthinkasido@gmail.com>
2026-03-27 16:59:13 +0000

    File changes

--- src/components/counterpoint/NoteGrid.jsx
+++ src/components/counterpoint/NoteGrid.jsx
@@ -945,7 +945,6 @@ export default function NoteGrid({
       if (deltaX < 40 && deltaY < 40) {
         // Just a click - clear selection
         setSelectedNotes(new Set());
-        onLoopChange?.(null, null);
       } else {
         // Calculate selected notes from marquee
         const startCell = getCellFromPosition(marquee.startX, marquee.startY);
